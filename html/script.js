709f4fe8e2aeb5520dffeaa67526e1c87f41e147
sebalp100 <sebalp100@gmail.com>
2022-10-17 19:37:12 -0300

    Add speakers mobile section style

--- html/script.js
+++ html/script.js
@@ -2,6 +2,7 @@ const menu = document.querySelector('.menu-button');
 const popUp = document.querySelector('.pop');
 const lecture = document.querySelector('.lecture');
 
+
 menu.addEventListener('click', () => {
     menu.classList.toggle('active');
     popUp.classList.toggle('active');
@@ -96,4 +97,89 @@ for (let i = 0; i < speakers.length; i += 1) {
     const positCard3 = document.createTextNode(speakers[i].description);
     subtextCard3.appendChild(positCard3);
     secondDiv.append(subtextCard3);
+
+    if (i === 1) {
+        const speakerButton = document.createElement('button');
+        speakerButton.classList = 'speakerButton';
+        const buttonText2 = document.createTextNode('MORE');
+        speakerButton.append(buttonText2);
+        document.getElementById('mainSection').appendChild(speakerButton);
+
+        const arrowIcon = document.createElement('img');
+        arrowIcon.className = 'arrowIcon';
+        arrowIcon.src = 'images/arrow.png';
+        speakerButton.append(arrowIcon);
+
+        const buttonS = document.querySelector('.speakerButton');
+
+        const speakerButton2 = document.createElement('button');
+        speakerButton2.classList = 'speakerButton2';
+        const buttonText3 = document.createTextNode('LESS');
+        speakerButton2.append(buttonText3);
+        document.getElementById('mainSection').appendChild(speakerButton2);
+
+        const arrowIcon2 = document.createElement('img');
+        arrowIcon2.className = 'arrowIcon2';
+        arrowIcon2.src = 'images/arrow2.png';
+        speakerButton2.append(arrowIcon2);
+
+        const buttonS2 = document.querySelector('.speakerButton2');
+
+        buttonS.addEventListener('click', () => {
+            const boxes = document.getElementsByClassName('mainContainer');
+            for (const box of boxes) {
+                box.classList.toggle('active');
+                speakerButton.style.order = 2;
+                speakerButton2.style.display = 'flex';
+                speakerButton2.style.order = 3;
+                speakerButton.style.display = 'none'
+            }
+        });
+
+        buttonS.addEventListener('click', () => {
+            const boxes2 = document.getElementsByClassName('mainContainer3');
+            const boxes3 = document.getElementsByClassName('mainContainer4');
+            for (const box of boxes2) {
+                box.classList.toggle('active');
+            }
+
+            for (const box of boxes3) {
+                if ((speakerButton.style.order = 2)) {
+                    box.classList.toggle('active');
+                }
+            }
+        });
+
+        buttonS2.addEventListener('click', () => {
+            const boxes2 = document.getElementsByClassName('mainContainer3');
+            const boxes3 = document.getElementsByClassName('mainContainer4');
+            for (const box of boxes2) {
+                box.classList.remove('active');
+                buttonS.style.display = 'flex';
+                buttonS2.style.display = 'none';
+            }
+
+            for (const box of boxes3) {
+                if ((speakerButton.style.order = 2)) {
+                    box.classList.remove('active');
+                }
+            }
+        });
+    }
+
+    if (i === 2) {
+        mainDiv.className = 'mainDiv3';
+        mainContainer.className = 'mainContainer3';
+    } else if (i === 3) {
+        mainDiv.className = 'mainDiv4';
+        mainContainer.className = 'mainContainer3';
+    } else if (i === 4) {
+        mainDiv.className = 'mainDiv5';
+        mainContainer.className = 'mainContainer4';
+    } else if (i === 5) {
+        mainDiv.className = 'mainDiv6';
+        mainContainer.className = 'mainContainer4';
+
+    }
+
 }
